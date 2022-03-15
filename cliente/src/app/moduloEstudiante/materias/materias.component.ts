066ffdf3f3520a867f4ce88694e1e812f8ca5852
pintocristian <pintocristian@unicauca.edu.co>
2022-03-15 13:02:54 -0500

    Actualizacion Front Estudiantes

--- cliente/src/app/moduloEstudiante/materias/materias.component.ts
+++ cliente/src/app/moduloEstudiante/materias/materias.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { finalize } from 'rxjs';
 import { AuthService } from 'src/app/service/service.service';
+import Swal from 'sweetalert2';
 import { VincularmateriaComponent } from '../vincularmateria/vincularmateria.component';
 
 @Component({
@@ -13,6 +14,7 @@ import { VincularmateriaComponent } from '../vincularmateria/vincularmateria.com
 })
 export class MateriasComponent implements OnInit {
 
+
   constructor(private authService: AuthService, private router: Router, private cookieService: CookieService, public dialog: MatDialog) { }
 
   public user$ = this.cookieService.get('Token_email');
@@ -20,33 +22,62 @@ export class MateriasComponent implements OnInit {
   public userPhoto$ = this.cookieService.get('Token_photo');
 
 
-  
+
 
   ngOnInit(): void {
+    localStorage.clear();
   }
 
   bandera: Boolean = false;
 
   codigo_planta: number;
-  verificarAgendamiento(codigo_planta:number) {
+
+  descargarGuia(codigo_planta: number) {
+    console.log("Entro a Descargar Guia Front")
+    this.authService.descargarGuia(codigo_planta).subscribe((result: any) => {
+      if (result == true) {
+        Swal.fire({
+          title: "¡Guia Descargada!",
+          text: "Revisa tu carpeta de Descargas.",
+          icon: "success",
+          confirmButtonColor: '#32408f',
+          confirmButtonText: 'Aceptar'
+        });
+      } else {
+        Swal.fire({
+          title: "¡ERROR!",
+          text: "No se ha encontrado una guia para descargar.",
+          icon: "error",
+          confirmButtonColor: '#32408f',
+          confirmButtonText: 'Aceptar'
+        });
+      }
+    });
+
+  }
+  verificarAgendamiento(codigo_planta: number) {
     this.codigo_planta = codigo_planta;
-    this.authService.saberCodigoGrupo().subscribe(respuesta=> {this.authService.verificarAgendamientoGrupo(respuesta,codigo_planta).pipe(finalize(() => this.prueba())).subscribe((result: any) => { this.bandera=result })
-  });
+    this.authService.saberCodigoGrupo().subscribe(respuesta => {
+      this.authService.verificarAgendamientoGrupo(respuesta, codigo_planta).pipe(finalize(() => this.prueba())).subscribe((result: any) => { this.bandera = result })
+    });
   }
 
-  prueba(){
+  prueba() {
     if (this.bandera) {
-      if(this.codigo_planta==1){
-        this.router.navigate(['/caidalibre']);
-      }else if(this.codigo_planta==2){
+      if (this.codigo_planta == 1) {
         this.router.navigate(['/leyhooke']);
-      }else if(this.codigo_planta==3){
+      } else if (this.codigo_planta == 2) {
+        this.router.navigate(['/caidalibre']);
+      } else if (this.codigo_planta == 3) {
         this.router.navigate(['/movparabolico']);
       }
-      
+
     } else {
-      alert('No tienes practicas agendadas en este horario');
-  
+      Swal.fire({
+        title: "¡ERROR!",
+        text: "No tienes practicas agendadas en este horario.",
+        icon: "error"
+      });
     }
   }
 
@@ -62,5 +93,29 @@ export class MateriasComponent implements OnInit {
       console.log(result);
       this.codigo = result;
     });
+  }
+
+  LeyHookeBotonOn() {
+    var uno: any;
+    uno = document.getElementById('LeyHookeBotonOn');
+    if (uno.textContent == '¡Descarga Completada!')
+      uno.textContent = 'Descargar Guia';
+    else uno.textContent = '¡Descarga Completada!';
+  }
+
+  MovParabolicoBotonOn() {
+    var dos: any;
+    dos = document.getElementById('MovParabolicoBotonOn');
+    if (dos.textContent == '¡Descarga Completada!')
+      dos.textContent = 'Descargar Guia';
+    else dos.textContent = '¡Descarga Completada!';
+  }
+
+  CaidaLibreBotonOn() {
+    var uno: any;
+    uno = document.getElementById('CaidaLibreBotonOn');
+    if (uno.textContent == '¡Descarga Completada!')
+      uno.textContent = 'Descargar Guia';
+    else uno.textContent = '¡Descarga Completada!';
   }
 }
